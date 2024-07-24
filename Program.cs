3923be894c86d92132f27716cb6ceb5f74b08456
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-24 10:56:38 +0900

    0724　10：56

--- Program.cs
+++ Program.cs
@@ -36,15 +36,44 @@ class Program
                 //新しいTaskクラスの生成を右辺で行い、左辺（配列）に代入
                 tasks[taskCount] = new Task { Id = nextId, TaskName = taskName, LimitTime = limitTime, Priority = priority };
 
-                taskCount++;
-                nextId++;
-
+                //次回に備える
+                taskCount++; //タスクの登録数を1件プラス
+                nextId++; //タスクのID番号も1プラス
             }
             else
             {
                 Console.WriteLine("タスクの登録上限に達しています");
             }
         }
+
+        //配列からタスクを削除するメソッド
+        public void DeleteTask(int id)
+        {
+            //自作メソッドであるFindTaskIndexByIdを呼び出し
+            //配列の各Idに一致するものがないかチェックして
+            //あれば該当の配列番号、なければ-1を取得
+            int index = FindTaskIndexById(id);
+        }
+
+        //配列の各Idに引数と一致するものがないかチェックして
+        //あれば該当の配列番号、なければ-1を取得
+        private int FindTaskIndexById(int id)
+        {
+            for (int i = 0; i < taskCount; i++)
+            {
+                if (tasks[i].Id == id)
+                {
+                    //一致するものがあれば
+                    //その時点でreturnしてメソッド終了
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+
+
 
     }
 
